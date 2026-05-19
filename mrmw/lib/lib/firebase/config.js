e6845dac5e77647746f26df32c2f1b96457c1aa1
RIYA <riyajoffy1@gmail.com>
2026-05-19 08:20:24 +0530

    fix: provide fallback client-side configuration in firebase initialization

--- mrmw/lib/lib/firebase/config.js
+++ mrmw/lib/lib/firebase/config.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || 'AIzaSyAgd91Z2keGkpcxf_boGtu6QAl0V2IYNoc',
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || 'mrmw-f56a1.firebaseapp.com',
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || 'mrmw-f56a1',
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || 'mrmw-f56a1.appspot.com',
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || '724990983667',
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || '1:724990983667:web:69588fa1002e45ea0a2686',
 };
 
 // Initialize Firebase only if it hasn't been initialized already
